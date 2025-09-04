41421bb0d3d69a976011ea84496d65fa6673fa3b
Eghosa Ordia <eghodia1004@yahoo.com>
2025-09-04 06:17:06 +0100

    feat: completed exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,55 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade Percentage?: ");
+        float gradePercentage = float.Parse(Console.ReadLine());
+
+        string gradeLetter = "";
+
+        if (gradePercentage >= 90)
+        {
+            gradeLetter = "A";
+
+        }
+        else if (gradePercentage >= 80)
+        {
+            gradeLetter = "B";
+
+        }
+        else if (gradePercentage >= 70)
+        {
+            gradeLetter = "C";
+
+        }
+        else if (gradePercentage >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else {
+            gradeLetter = "F";
+        }
+
+        string sign = "";
+        int firstDecimalDigit = (int)(gradePercentage % 10);
+        if (firstDecimalDigit >= 7 && gradeLetter != "A" && gradeLetter != "F")
+        {
+            sign = "+";
+        }
+        else if (firstDecimalDigit <= 3 && gradeLetter != "F")
+        {
+            sign = "-";
+        }
+        gradeLetter += sign;
+        Console.WriteLine($"You got a {gradeLetter}.");
+        
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class. Better luck next time!");
+        }
     }
 }
