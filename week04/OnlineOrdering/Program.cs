4b37e786980303743b0775d7cf8b3db6195d0715
Eghosa Ordia <eghodia1004@yahoo.com>
2025-09-27 07:50:32 +0100

    status: project init

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        // Once you have created these classes, write a program that creates at least two orders with a 2-3 products each. Call the methods to get the packing label, the shipping label, and the total price of the order, and display the results of these methods.
     }
 }
