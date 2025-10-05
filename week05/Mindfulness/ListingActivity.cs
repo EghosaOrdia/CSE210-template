cb4c8248fb59221ae8eabced120a96b66937a6a2
Eghosa Ordia <eghodia1004@yahoo.com>
2025-10-05 13:43:04 +0100

    fix: correct class name typo and enhance StartActivity method with prompts and descriptions

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,4 +1,34 @@
 public class LisitngActivity : Base
 {
-    
+    private List<string> _prompts;
+    private Random _randomGenerator;
+    public LisitngActivity()
+    {
+        _activity = "Listing";
+        _randomGenerator = new Random();
+    }
+    public void StartActivity()
+    {
+        string welcome = GetWelcomeMessage(_activity);
+        Console.WriteLine(welcome);
+
+        string description = GetDescription("This activity will help you reflect on good things in your life by having you list as many things as you can in a certain area.\n");
+        Console.WriteLine(description);
+
+        SetDuration();
+        Console.Clear();
+
+        Console.Write("Get ready...");
+        SpinnerAnimation(3000);
+        Console.Clear();
+
+        _endTime = DateTime.Now.AddSeconds(_duration);
+
+        while (DateTime.Now < _endTime)
+        {
+            Console.WriteLine("List as many things as you can to the following prompts:");
+        }
+
+    }
+
 }
